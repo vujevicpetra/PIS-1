667a7260bce50a6a8731e7289887ad543ad69d2e
vujevicpetra <pvujev00@fesb.hr>
2020-04-30 14:24:21 +0200

    Merged

--- vjezba1/NewClass.cs
+++ vjezba1/NewClass.cs
@@ -8,14 +8,11 @@ namespace vjezba1
 {
     class NewClass
     {
-<<<<<<< HEAD
         static void MyMethod()
         {
 
         }
             
-=======
         Console.WriteLine("Petrina metoda");
->>>>>>> 567ff37cf67707922e473fe51dda212e55b6e051
     }
 }
